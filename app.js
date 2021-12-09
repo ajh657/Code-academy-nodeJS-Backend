75297bc7a075e012f3f6a1c5299cbf6e5c3c487f
Arttu Höylä <33629266+ajh657@users.noreply.github.com>
2021-12-09 14:10:04 +0200

    TH 2

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+var router = express.Router();
 
 //return index.html
 app.get("/", (req, res) => {
@@ -10,6 +11,22 @@ app.get("/", (req, res) => {
 app.get("/mock-data", (req, res) => {
   res.sendFile(__dirname + "/MOCK_DATA.json");
 });
+
+app.use("/hello", (req, res, next) => {
+  console.log("Hello");
+  next();
+});
+
+app.use("/world", (req, res, next) => {
+  console.log("World");
+  next();
+});
+
+router.get("/mikkihiiri", (req, res) => {
+  res.send("router");
+});
+
+app.use("/akuankka", router);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
