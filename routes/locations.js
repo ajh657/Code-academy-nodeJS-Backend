148b08174079127971b9c78218f3940cf45adad0
Arttu Höylä <33629266+ajh657@users.noreply.github.com>
2021-12-09 14:36:22 +0200

    TH 5

--- routes/locations.js
+++ routes/locations.js
@@ -4,13 +4,20 @@ var locations = [];
 
 router.get("/location/:id", function (req, res) {
   var id = req.params.id;
-  res.send("You requested the location with id: " + id);
+  var location = locations.find(function (location) {
+    return location.id === id;
+  });
+  if (location != undefined) {
+    res.json(location);
+  } else {
+    res.sendStatus(404);
+  }
 });
 
 router.post("/location", function (req, res) {
   var location = req.body;
   locations.push(location);
-  res.send(location);
+  res.status(201).send("Location created");
 });
 
 router.delete("/location/:id", function (req, res) {
