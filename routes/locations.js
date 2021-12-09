df37cec7433279021f3f8bc84c66434ecbd29823
Arttu Höylä <33629266+ajh657@users.noreply.github.com>
2021-12-09 14:43:07 +0200

    TH6

--- routes/locations.js
+++ routes/locations.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var locations = [];
 
 router.get("/location/:id", function (req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
   var id = req.params.id;
   var location = locations.find(function (location) {
     return location.id === id;
@@ -15,12 +16,14 @@ router.get("/location/:id", function (req, res) {
 });
 
 router.post("/location", function (req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
   var location = req.body;
   locations.push(location);
   res.status(201).send("Location created");
 });
 
 router.delete("/location/:id", function (req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
   var id = req.params.id;
   locations = locations.filter(function (location) {
     return location.id !== id;
